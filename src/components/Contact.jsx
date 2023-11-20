35cbe39e45a63c8d6dc289b4ae019b6a19c72be0
Pennymike <gbanyamichael1@gmail.com>
2023-11-21 04:56:14 +1100

    fixed margin

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,7 +10,7 @@ export default function Contact() {
 
   return (
     <>
-      <div className=" mb-72 md:mb-[25rem] text-sm md:text-lg max-w-xl mx-auto">
+      <div className=" mb-72 pt-40 md:mb-[25rem] text-sm md:text-lg max-w-xl mx-auto">
         <div className="mx-3">
           <p>Thank you for wanting to contact me.</p>
           <p>
@@ -18,19 +18,23 @@ export default function Contact() {
             download my CV below to access my contact information.
           </p>
         </div>
-        <div className="flex flex-shrink-0 justify-center">
+        <div className="pt-10">
           <button
             onClick={handleShowDownloadSect}
             className={`${
               showDownloadSect
-                ? "bg-violet-600 hover:bg-violet-400"
-                : "bg-red-600 hover:bg-red-400"
-            } my-5 ml-3 text-white p-2 md:p-3 rounded-lg w-fit font-bold`}
+                ? "bg-violet-600/90 hover:bg-violet-400"
+                : "bg-red-600/90 hover:bg-red-400"
+            } my-5 ml-3 text-white p-2 md:p-3 rounded-lg w-fit font-semibold`}
           >
             {showDownloadSect ? "Download CV" : "Cancel download"}
           </button>
         </div>
-        <div className={showDownloadSect ? "hidden" : "block"}>
+        <div
+          className={`animate__animated animate__fadeInTopRight ${
+            showDownloadSect ? "hidden" : "block"
+          }`}
+        >
           <DownloadCv />
         </div>
       </div>
